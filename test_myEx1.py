044daeb4118f59ff18e3c367d57f97ccc410e588
gtlali <93737436+gtlali@users.noreply.github.com>
2021-11-17 22:13:57 +0530

    Update test_myEx1.py

--- test_myEx1.py
+++ test_myEx1.py
@@ -1,10 +1,10 @@
 import pytest
-def Test_myEx1_1():
+def test_myEx1_1():
 	x=5
 	y=6
 	assert x+1 == y,"test true"
 	assert x == y,"test failed"
-def Test_myEx1_2():
+def test_myEx1_2():
 	x=5
 	y=6
 	assert x+1 == y,"test true" 
